5ccfbecda8b270cb93d735ea8cae50b49f1878a0
jasonvanwyk <jason@sondelastore.co.za>
2019-03-01 17:27:55 +0200

    add python file

---
+++ ConnectToPostgresql.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import psycopg2
+import sys
+import datetime
+import time
+import random
+import serial
+
+
+con = None
+ 
+for loop in range(0,10):
+    try:
+        x = random.randint(1,101)
+        t = datetime.datetime.now() 
+        con = psycopg2.connect("host='localhost' dbname='Jason' user='postgres' password='S0nd3l@1'")   
+        cur = con.cursor()
+        #cur.execute("INSERT INTO tempdata VALUES(now(), %(x))")
+        cur.execute("INSERT INTO tempdata VALUES (%s, %s)", (x, t,))
+        con.commit()
+
+     
+    finally:   
+        if con:
+            con.close()
+
+    print(x) 
+    time.sleep(1)
